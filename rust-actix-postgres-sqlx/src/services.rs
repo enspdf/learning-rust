d5e0ddf76c140256a11785cfdbac85fbbce9c9b0
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2022-10-30 23:03:43 -0500

    sqlx api example

---
+++ rust-actix-postgres-sqlx/src/services.rs
@@ -0,0 +1,78 @@
+use crate::AppState;
+use actix_web::{
+    get, post,
+    web::{Data, Json, Path},
+    HttpResponse, Responder,
+};
+use serde::{Deserialize, Serialize};
+use sqlx::{self, FromRow};
+
+#[derive(Serialize, FromRow)]
+struct User {
+    id: i32,
+    first_name: String,
+    last_name: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct Article {
+    id: i32,
+    title: String,
+    content: String,
+    created_by: i32,
+}
+
+#[derive(Deserialize)]
+pub struct CreateArticleBody {
+    pub title: String,
+    pub content: String,
+}
+
+#[get("/users")]
+pub async fn fetch_users(state: Data<AppState>) -> impl Responder {
+    // "GET /users".to_string()
+    match sqlx::query_as::<_, User>("SELECT id, first_name, last_name FROM users")
+        .fetch_all(&state.db)
+        .await
+    {
+        Ok(users) => HttpResponse::Ok().json(users),
+        Err(_) => HttpResponse::NotFound().json("No users found"),
+    }
+}
+
+#[get("/users/{id}/articles")]
+pub async fn fetch_user_articles(state: Data<AppState>, path: Path<i32>) -> impl Responder {
+    let id: i32 = path.into_inner();
+    // format!("GET /users/{id}/articles")
+
+    match sqlx::query_as::<_, Article>(
+        "SELECT id, title, content, created_by FROM articles WHERE created_by = $1",
+    )
+    .bind(id)
+    .fetch_all(&state.db)
+    .await
+    {
+        Ok(articles) => HttpResponse::Ok().json(articles),
+        Err(_) => HttpResponse::NotFound().json("No articles found"),
+    }
+}
+
+#[post("/users/{id}/articles")]
+pub async fn create_user_article(
+    state: Data<AppState>,
+    path: Path<i32>,
+    body: Json<CreateArticleBody>,
+) -> impl Responder {
+    let id: i32 = path.into_inner();
+    // format!("POST /users/{id}/articles")
+
+    match sqlx::query_as::<_, Article>("INSERT INTO articles (title, content, created_by) VALUES ($1, $2, $3) RETURNING id, title, content, created_by")
+    .bind(body.title.to_string())
+    .bind(body.content.to_string())
+    .bind(id)
+    .fetch_one(&state.db)
+    .await {
+        Ok(article) => HttpResponse::Ok().json(article),
+        Err(_) => HttpResponse::InternalServerError().json("Failer to create user article")
+    }
+}
