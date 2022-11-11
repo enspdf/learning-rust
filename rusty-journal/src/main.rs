41a721ade9eac2959b27ba99cfe84cfd6be5e840
Sebastian Higuita <sebastianhiguitaca@gmail.com>
2022-11-11 18:29:27 -0500

    rust projects & rusty journal

---
+++ rusty-journal/src/main.rs
@@ -0,0 +1,25 @@
+use structopt::StructOpt;
+
+mod cli;
+mod tasks;
+
+use {
+    cli::{Action::*, CommandLineArgs},
+    tasks::Task,
+};
+
+fn main() {
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
+}
